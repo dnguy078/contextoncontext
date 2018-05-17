9ff412f54e8b26265c5fd00fd1ed3def8bf66679
Dennis Nguyen <dnguy078@ucr.edu>
2018-05-17 10:06:59 -0700

    chain pic

--- context_chain_example.go
+++ context_chain_example.go
@@ -9,6 +9,6 @@ func tree() {
 	ctx := context.Background()
 	ctx2, _ := context.WithCancel(ctx)
 	ctx3, _ := context.WithTimeout(ctx2, time.Second*5)
-	ctx4, _ := context.WithTimeout(ctx3, time.Second*4)
-	ctx5, _ := context.WithValue(ctx3, "userID", 12)
+	ctx4, _ := context.WithTimeout(ctx2, time.Second*4)
+	ctx5, _ := context.WithValue(ctx4, "userID", 12)
 }
